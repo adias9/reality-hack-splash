66bc83003378b474954af32024b961aef16b5996
adias9 <andreasdias9@gmail.com>
2019-10-16 21:03:01 -0400

    remove console warnings and add better email regex

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import logoImg from './img/RHack-BlackTransparent-01.png'
 import logoImg2 from './img/RHack-WhiteOnBlackBG-01.png'
 import logoImg3 from './img/rhackSmall.png'
 import logoImg4 from './img/clubLog.png'
-import defaultImg from './img/default.svg';
+// import defaultImg from './img/default.svg';
 import {
     eswar, sarah, scott,
     maria, brian, andreas,
@@ -77,31 +77,31 @@ function Letter() {
                   </Col>
                   <Col xs={12} sm={12} md={{span: 7, offset: 1}}>
                       <h2>A Letter to the Community</h2>
-                      <p>The MIT XR Hackathon is back for 2020, and coming at you in a <b>BIG</b> way.... 	&#x1F4E2;		&#x1F389;</p>
+                      <p>The MIT XR Hackathon is back for 2020, and coming at you in a <b>BIG</b> way.... 	<span role="img" aria-label="airhorn">&#x1F4E2;</span>		<span role="img" aria-label="party-popper">&#x1F389;</span></p>
 
                       <p>Since inception, this hackathon has produced over <b>300 projects</b> and helped <b>1,500</b> participants realize the potential to become XR creators. Year over year we bring together close to 500 global, magnetic, fun do-ers, to learn, create, and hack a better future.</p>
 
-                      <p>Entering our fourth year, we have grown larger than we imagined possible. Startups have been founded (and even acquired!), countless XR careers jump-started, and a plentitude of collaborations propelled into the future.  &#128640;</p>
+                      <p>Entering our fourth year, we have grown larger than we imagined possible. Startups have been founded (and even acquired!), countless XR careers jump-started, and a plentitude of collaborations propelled into the future.  <span role="img" aria-label="rocket">&#128640;</span></p>
 
-                      <p>Last year was unreal. It took our breath away to see the energy and the buzz from participants to sponsors to tech leaders chatting, friending, reminiscing, and still hacking for months afterwards. We realized the magnitude and impact we had. So this year we took the initiative to reexamine what this event means and reimagine its structure and future. We're ready to bring 2020 to new heights, and continue growing and driving the community. We're welcoming amazing partners, to push spatial computing forwards. We're working with industry giants and their leaders to bring the latest and greatest technologies into the hands of builders and dreamers from around the globe. Lastly, we're constantly listening and iterating to bring together this diverse group of designers, developers, artists, makers, and explorers. &#x1F30E;</p>
+                      <p>Last year was unreal. It took our breath away to see the energy and the buzz from participants to sponsors to tech leaders chatting, friending, reminiscing, and still hacking for months afterwards. We realized the magnitude and impact we had. So this year we took the initiative to reexamine what this event means and reimagine its structure and future. We're ready to bring 2020 to new heights, and continue growing and driving the community. We're welcoming amazing partners, to push spatial computing forwards. We're working with industry giants and their leaders to bring the latest and greatest technologies into the hands of builders and dreamers from around the globe. Lastly, we're constantly listening and iterating to bring together this diverse group of designers, developers, artists, makers, and explorers. <span role="img" aria-label="earth">&#x1F30E;</span></p>
 
                       <p>The MIT XR Hackathon is not just an ordinary hackathon. From the workshops, to the whirlwind team formation, wild ideas, hands on mentorship, crazy amounts of hardware, killer partying, and more, it fosters a unique commmunity.</p>
 
                       <p>And for 2020, we're ready! Today we're excited to relaunch and reveal our new brand to the world. With new colors, a new name, and a fresh face- we hope you can see our visions for magic making and impact creation, as we continue to push bright minds.</p>
 
-                      <p>Big thanks to our friends in the community (shout-out to <a target="_blank" href="http://www.riclebre.com/">Ric Lebre</a> and <a target="_blank" href="http://www.thisisplanecrazy.com/">Plane Crazy Design studio</a>, who helped create our fun and fresh 2020 rebrand: hack the now and build a better later.</p>
+                      <p>Big thanks to our friends in the community (shout-out to <a rel="noopener noreferrer" target="_blank" href="http://www.riclebre.com/">Ric Lebre</a> and <a rel="noopener noreferrer" target="_blank" href="http://www.thisisplanecrazy.com/">Plane Crazy Design studio</a>, who helped create our fun and fresh 2020 rebrand: hack the now and build a better later.</p>
 
 
-                      <p>Reality Hack 2020 is coming back to MIT January 16th through the 20th, hosted at the MIT Media Lab. Our team of organizers, both from <a target="_blank" href="http://vratmit.com">VR/AR@MIT</a> as well as the Reality Hack Organization are extremely excited to build this out. We hope to see you here in January, as we <i>hack-to-the-future</i>. If you're interested in collaborating with us, joining our team, sponsoring, or just shooting some ideas, feel free to send us a note at realityhack@mit.edu. The application period will start very soon, so keep an eye out! We hope you're as excited as we are!</p>
+                      <p>Reality Hack 2020 is coming back to MIT January 16th through the 20th, hosted at the MIT Media Lab. Our team of organizers, both from <a rel="noopener noreferrer" target="_blank" href="http://vratmit.com">VR/AR@MIT</a> as well as the Reality Hack Organization are extremely excited to build this out. We hope to see you here in January, as we <i>hack-to-the-future</i>. If you're interested in collaborating with us, joining our team, sponsoring, or just shooting some ideas, feel free to send us a note at realityhack@mit.edu. The application period will start very soon, so keep an eye out! We hope you're as excited as we are!</p>
                       <br/>
                       <p>—Reality Hack team</p>
 
 
-                      <p>#HackToTheFuture 	&#x1F64C;
+                      <p>#HackToTheFuture 	<span role="img" aria-label="hands-up">&#x1F64C;</span>
                       <br></br>
-                      #RHack 	&#x1F52E;
+                      #RHack 	<span role="img" aria-label="crystal-ball">&#x1F52E;</span>
                       <br></br>
-                      #RealityHack &#x1F525;
+                      #RealityHack <span role="img" aria-label="fire">&#x1F525;</span>
                       </p>
                   </Col>
               </Row>
@@ -141,7 +141,8 @@ function Main() {
         }
     }
     const addEmailToMailinglist = () => {
-        let mailformat = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
+        let mailformat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        // /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
         if (!mailformat.test(email)) {
             setShowErr("Please enter a valid email");
             return
