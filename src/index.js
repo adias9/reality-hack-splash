eea2bfc703ab119b7ce39f6c9cd78fabbfa73ee0
adias9 <andreasdias9@gmail.com>
2019-10-22 20:11:20 -0400

    Add Https redirect

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HttpsRedirect from 'react-https-redirect';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HttpsRedirect>
+        <App />
+    </HttpsRedirect>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
