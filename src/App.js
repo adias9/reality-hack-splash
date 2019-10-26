e7a19001f87dbad41cc80bc8d9d66a1a844cf9df
adias9 <andreasdias9@gmail.com>
2019-10-26 15:19:54 -0400

    Add Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ReactGA from 'react-ga';
 import axios from 'axios';
 import {
     Button,
@@ -26,6 +27,9 @@ import {
     michael, elena, vik,
     elizabeth, divyanshu, kealani
 } from './img/headshots/';
+
+ReactGA.initialize(`${process.env.REACT_APP_GA_TRACKING_ID}`);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function Team() {
     const teamMap = [
